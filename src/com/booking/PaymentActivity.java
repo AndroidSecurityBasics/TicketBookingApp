ea1eb81b53663cedc7fae687898ea230dfbd7353
NazneenRupawalla <nazneenr@thoughtworks.com>
2015-07-05 22:10:23 +0530

    Using Local Broadcast Manager to secure the app.

--- src/com/booking/PaymentActivity.java
+++ src/com/booking/PaymentActivity.java
@@ -1,21 +1,39 @@
 package com.booking;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.view.View;
+import android.widget.Toast;
+
 
 public class PaymentActivity extends Activity {
+    private final static String PAYMENT_ACTION = "com.booking.action.PAY";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment);
+        LocalBroadcastManager.getInstance(this).registerReceiver(userDefinedBroadcastReceiver, new IntentFilter(PAYMENT_ACTION));
+        registerReceiver(userDefinedBroadcastReceiver, new IntentFilter(PAYMENT_ACTION));
+
     }
 
     public void makePayment(View view) {
         Intent intent = new Intent();
-        intent.setAction("com.booking.action.PAY");
-        intent.setPackage("com.booking");
-        sendBroadcast(intent);
+        intent.setAction(PAYMENT_ACTION);
+        LocalBroadcastManager localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        localBroadcastManager.sendBroadcast(intent);
     }
+
+    private BroadcastReceiver userDefinedBroadcastReceiver = new BroadcastReceiver() {
+
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "You are in the same app...SAFE!!!", Toast.LENGTH_LONG).show();
+        }
+    };
 }
