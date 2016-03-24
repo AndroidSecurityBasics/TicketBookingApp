8d7f111eeacbda519c212f61becc971a69504b6a
NazneenRupawalla <nazneenr@thoughtworks.com>
2016-03-23 23:55:35 -0400

    UI enhancement
    - Version updated

--- app/src/main/java/com/booking/PaymentActivity.java
+++ app/src/main/java/com/booking/PaymentActivity.java
@@ -1,23 +1,26 @@
 package com.booking;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
 
 
-public class PaymentActivity extends Activity {
+public class PaymentActivity extends AppCompatActivity {
     private final static String PAYMENT_ACTION = "com.booking.action.PAY";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.payment);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void makePayment(View view) {
--- app/src/main/java/com/booking/TicketBookingActivity.java
+++ app/src/main/java/com/booking/TicketBookingActivity.java
@@ -1,15 +1,20 @@
 package com.booking;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class TicketBookingActivity extends Activity {
+public class TicketBookingActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.book);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void bookTickets(View view) {
